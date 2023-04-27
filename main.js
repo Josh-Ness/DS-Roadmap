57a58b55bc09153eb9d9fe54d06f979fc0cd4aca
Josh <joshua.s.ness@ndsu.edu>
2023-04-27 00:09:14 -0500

    Connected remaining nodes

--- main.js
+++ main.js
@@ -65,34 +65,10 @@ const data = [
     { id: "Kafka", group: 10 },
     { id: "Hive", group: 10 },
     { id: "Pig", group: 10 },
-
-        // Natural Language Processing
-    { source: "Text preprocessing", target: "Text classification", value: 1 },
-    { source: "Text preprocessing", target: "Sentiment analysis", value: 1 },
-    { source: "Text classification", target: "Topic modeling", value: 1 },
-    { source: "Sentiment analysis", target: "Named Entity Recognition", value: 1 },
-
-    // Time Series Analysis
-    { source: "Time series decomposition", target: "Autoregression models", value: 1 },
-    { source: "Time series decomposition", target: "Moving average models", value: 1 },
-    { source: "Autoregression models", target: "Exponential smoothing state space models", value: 1 },
-    { source: "Moving average models", target: "Forecasting", value: 1 },
-
-    // Data Engineering
-    { source: "Data storage (SQL, NoSQL)", target: "Data pipelines", value: 1 },
-    { source: "Data pipelines", target: "Data integration", value: 1 },
-    { source: "Data integration", target: "Data warehousing", value: 1 },
-    { source: "Data warehousing", target: "ETL processes", value: 1 },
-
-    // Big Data Technologies
-    { source: "Hadoop", target: "Spark", value: 1 },
-    { source: "Hadoop", target: "Kafka", value: 1 },
-    { source: "Spark", target: "Hive", value: 1 },
-    { source: "Kafka", target: "Pig", value: 1 },
   ];
   
   const links = [
-      // Programming Languages
+    // Programming Languages
   { source: "Python", target: "Data wrangling", value: 1 },
   { source: "Python", target: "Matplotlib", value: 1 },
   { source: "Python", target: "Seaborn", value: 1 },
@@ -107,6 +83,8 @@ const data = [
   { source: "R", target: "Supervised learning", value: 1 },
   { source: "R", target: "Unsupervised learning", value: 1 },
   { source: "R", target: "Reinforcement learning", value: 1 },
+
+  { source: "SQL", target: "Data storage (SQL, NoSQL)", value: 1 },
 
   // Data Cleaning and Preprocessing
   { source: "Data wrangling", target: "Handling missing data", value: 1 },
@@ -132,18 +110,57 @@ const data = [
   { source: "Reinforcement learning", target: "Model evaluation and validation", value: 1 },
   { source: "Model evaluation and validation", target: "Ensemble methods", value: 1 },
 
-  // Deep Learning
-  { source: "Artificial Neural Networks", target: "Convolutional Neural Networks", value: 1 },
-  { source: "Artificial Neural Networks", target: "Recurrent Neural Networks", value: 1 },
-  { source: "Convolutional Neural Networks", target: "Autoencoders", value: 1 },
-  { source: "Recurrent Neural Networks", target: "Autoencoders", value: 1 },
-  { source: "Autoencoders", target: "Generative Adversarial Networks", value: 1 },
+   // Deep Learning
+   { source: "Artificial Neural Networks", target: "Convolutional Neural Networks", value: 1 },
+   { source: "Artificial Neural Networks", target: "Recurrent Neural Networks", value: 1 },
+   { source: "Convolutional Neural Networks", target: "Autoencoders", value: 1 },
+   { source: "Recurrent Neural Networks", target: "Autoencoders", value: 1 },
+   { source: "Autoencoders", target: "Generative Adversarial Networks", value: 1 },
+ 
+   // Natural Language Processing
+   { source: "Text preprocessing", target: "Text classification", value: 1 },
+   { source: "Text preprocessing", target: "Sentiment analysis", value: 1 },
+   { source: "Text preprocessing", target: "Topic modeling", value: 1 },
+   { source: "Text classification", target: "Named Entity Recognition", value: 1 },
+ 
+   // Time Series Analysis
+   { source: "Time series decomposition", target: "Autoregression models", value: 1 },
+   { source: "Time series decomposition", target: "Moving average models", value: 1 },
+   { source: "Time series decomposition", target: "Exponential smoothing state space models", value: 1 },
+   { source: "Time series decomposition", target: "Forecasting", value: 1 },
+ 
+   // Data Engineering
+   { source: "Data storage (SQL, NoSQL)", target: "Data pipelines", value: 1 },
+   { source: "Data storage (SQL, NoSQL)", target: "Data integration", value: 1 },
+   { source: "Data pipelines", target: "Data warehousing", value: 1 },
+   { source: "Data warehousing", target: "ETL processes", value: 1 },
+ 
+   // Big Data Technologies
+   { source: "Hadoop", target: "Spark", value: 1 },
+   { source: "Hadoop", target: "Kafka", value: 1 },
+   { source: "Hadoop", target: "Hive", value: 1 },
+   { source: "Hadoop", target: "Pig", value: 1 },
+   { source: "Spark", target: "Kafka", value: 1 },
+   { source: "Spark", target: "Hive", value: 1 },
+   { source: "Spark", target: "Pig", value: 1 },
+   { source: "Kafka", target: "Hive", value: 1 },
+   { source: "Kafka", target: "Pig", value: 1 },
+   { source: "Hive", target: "Pig", value: 1 },
 
-
+   // Other Links
+   { source: "Forecasting", target: "Supervised learning", value: 1 },
+   { source: "Feature scaling", target: "Descriptive statistics", value: 1 },
+  { source: "Tableau", target: "Descriptive statistics", value: 1 },
+  { source: "Named Entity Recognition", target: "Artificial Neural Networks", value: 1 },
+  { source: "Forecasting", target: "Supervised learning", value: 1 },
+  { source: "ETL processes", target: "Data storage (SQL, NoSQL)", value: 1 },
+  { source: "Pig", target: "Data storage (SQL, NoSQL)", value: 1 },
+  { source: "Bayesian statistics", target: "Hypothesis testing", value: 1 },
   ];
   
-  const width = 1000;
-  const height = 800;
+  const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+  const width = window.innerWidth - margin.left - margin.right;
+  const height = window.innerHeight - margin.top - margin.bottom;
   
   const color = d3.scaleOrdinal(d3.schemeCategory10);
   
@@ -154,13 +171,15 @@ const data = [
       d3.forceLink(links).id((d) => d.id)
     )
     .force("charge", d3.forceManyBody().strength(-500))
-    .force("center", d3.forceCenter(width / 2, height / 2));
+    .force("center", d3.forceCenter(width / 2, height / 2))
+    .force("collide", d3.forceCollide(30))
   
-  const svg = d3
+    const svg = d3
     .select("body")
     .append("svg")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
   
   const link = svg
     .selectAll("line")
@@ -176,10 +195,11 @@ const data = [
     .join("circle")
     .attr("r", 15)
     .attr("fill", (d) => color(d.group))
-    .call(drag(simulation));
-  
+    .call(drag(simulation))
+    .on("click", (event, d) => showInfo(event, d));
   node.append("title").text((d) => d.id);
-  
+  svg.on("click", (event) => hideInfo(event));
+
   simulation.on("tick", () => {
     link
       .attr("x1", (d) => d.source.x)
@@ -187,7 +207,8 @@ const data = [
       .attr("x2", (d) => d.target.x)
       .attr("y2", (d) => d.target.y);
   
-    node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+      node.attr("cx", (d) => (d.x = Math.max(15, Math.min(width - 15, d.x))))
+      .attr("cy", (d) => (d.y = Math.max(15, Math.min(height - 15, d.y))));
   });
   
   function drag(simulation) {
@@ -213,4 +234,18 @@ const data = [
       .on("drag", dragged)
       .on("end", dragended);
   }
+
+  function showInfo(event, d) {
+    event.stopPropagation();
+    const infoBox = d3.select("#info-box");
+    infoBox.style("display", "block");
+    infoBox.html(`
+      <h4>${d.id}</h4>
+      <p>Group: ${d.group}</p>
+    `);
+  }
   
+  function hideInfo(event) {
+    event.stopPropagation();
+    d3.select("#info-box").style("display", "none");
+  }
